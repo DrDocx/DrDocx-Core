b2a40085cfaf87807f965ddee5d7368b0c89dcd1
Faiz <faiz.surani@gmail.com>
2020-01-10 22:30:15 -0800

    Added EntityFrameworkCore and model classes

---
+++ DrDocx-Core/Models/GPatientStructures.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrDocx_Core.Models
+{
+
+    /// <summary>
+    /// Summary description for DPatient
+    /// </summary>
+    ///
+    /// THIS WILL NOT WORK TIL EVERYTHINGS IN A PROJECT BECAUSE C# IS SPECIAL 
+    public class DPatient
+    {
+        public List<DTestResultGroup> ResultGroups { get; set; }
+        public DPatientData Data { get; set; }
+        public string Diagnosis { get; set; }
+        public int Id { get; set; }
+    }
+
+    public struct DPatientData
+    {
+        public string Name { get; set; }
+        public string PreferredName { get; set; }
+        public string Address { get; set; }
+        public string Medications { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfTesting { get; set; }
+        public string Notes { get; set; }
+        public int MedicalRecordNumber { get; set; }
+        public int AgeAtTesting { get; set; }
+    }
+
+    public struct DTestResultGroup
+    {
+        public DTestGroup Data { get; set; }
+        public List<DTestResult> Tests { get; set; }
+
+        public int Id { get; set; }
+    }
+
+    public struct DTestResult
+    {
+        public int RawScore { get; set; }
+        public int ScaledScore { get; set; }
+        public int ZScore { get; set; }
+        public int Percentile { get; set; }
+        public DTest RelatedTest { get; set; }
+
+        public int ID { get; set; }
+    }
+}
---
+++ DrDocx-Core/Models/GTestStructures.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrDocx_Core.Models
+{
+    public struct DTest
+    {
+        public string TestName;
+        public string Description;
+
+        public int Id;
+
+        //public DTest(string name, string description)
+        //{
+        //    TestName = name;
+        //    Description = description;
+        //}
+    }
+
+    public struct DTestGroup
+    {
+        public string Name;
+        public string Description;
+        public List<DTest> Tests;
+
+        public int Id;
+
+        // This can be used for when a test group doesn't have tests ready to populate it with
+        //public DTestGroup(string name, string description, int id)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //    Tests = null;
+        //    Id = id;
+        //}
+
+        //public DTestGroup(string name, string description, List<DTest> tests, int id)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //    Tests = tests;
+        //    Id = id;
+        //}
+    }
+}
